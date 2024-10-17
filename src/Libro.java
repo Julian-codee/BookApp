27c2ff54a0136794e06310abe4c4c338698db52a
Julian Cortes <mmm808383@gmail.com>
2024-10-17 10:45:54 -0500

    Creacion de la clase Libro

---
+++ src/Libro.java
@@ -0,0 +1,33 @@
+import java.util.Date;
+
+public class Libro {
+    private int id;
+    private String title;
+    private String author;
+    private Date yearPublication;
+
+    //Constructor de la clase
+
+    public Libro(int id, String title, String auhtor, Date yearPublication){
+        this.id = id;
+        this.title = title;
+        this.author = auhtor;
+        this.yearPublication = yearPublication;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Date getYearPublication() {
+        return yearPublication;
+    }
+}
